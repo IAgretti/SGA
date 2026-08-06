fc7d4fc156df839f94d87914de1c3d13cb4b3482
IAgretti <irinagretti@gmail.com>
2026-08-06 15:46:45 -0300

    Clase 04: Fetch y Api

--- JS/alumnos.js
+++ JS/alumnos.js
@@ -1,26 +1,46 @@
-const alumnos = [
-    {
-        id: 1,
-        nombre: "Ana"
-    },
-    {
-        id: 2,
-        nombre: "José"
-    }
-];
-function obtenerAlumnos(){
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(alumnos)
-        }, 2000);
-    })
-}
-async function iniciar(){
-    const datos = await obtenerAlumnos()
-    console.table(datos)
-}
-iniciar()
+// const alumnos = [
+//     {
+//         id: 1,
+//         nombre: "Ana"
+//     },
+//     {
+//         id: 2,
+//         nombre: "José"
+//     }
+// ];
+// function obtenerAlumnos(){
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve(alumnos)
+//         }, 2000);
+//     })
+// }
 
 // crear obtenerMaterias()
 // crear obtenerDocentes()
 // mostrar los datos a través de async/await
+
+async function obtenerAlumnos() {
+    const respuesta = await fetch("https://jsonplaceholder.typicode.com/users")
+    const alumnos = await respuesta.json()
+    return alumnos
+}
+
+function mostrarAlumnos(alumnos){
+   console.log(alumnos)
+//    console.log(alumnos[5])
+// for (const alumno of alumnos){
+//     console.log(alumno.id, alumno.name, alumno.email)
+// }
+}
+
+async function inciar(){
+    const alumnos = await obtenerAlumnos()
+    mostrarAlumnos(alumnos)
+}
+
+inciar()
+
+// /post
+// /comments
+// id, título, usuario
--- JS/asincronia.js
+++ JS/asincronia.js
@@ -36,111 +36,117 @@
 
 // console.log("El usuario puede seguir navegando")
 
-function obtenerAlumnos(){
-   return new Promise((resolve) => {
-    setTimeout(() => {
-        console.log("Ya tengo el arreglo")
-        resolve(["Ana", "Juan", "Pedro"])
-    }, 3000);
-   })
-}
-// obtenerAlumnos().then((alumnos) => {
-//     console.log(alumnos)
-// })
+// function obtenerAlumnos(){
+//    return new Promise((resolve) => {
+//     setTimeout(() => {
+//         console.log("Ya tengo el arreglo")
+//         resolve(["Ana", "Juan", "Pedro"])
+//     }, 3000);
+//    })
+// }
+// // obtenerAlumnos().then((alumnos) => {
+// //     console.log(alumnos)
+// // })
 
-// async function iniciar() {
-//     const alumnos = await obtenerAlumnos()
-//     console.log(alumnos)
-// } 
-// iniciar()
+// // async function iniciar() {
+// //     const alumnos = await obtenerAlumnos()
+// //     console.log(alumnos)
+// // } 
+// // iniciar()
 
-// login(usuario).then((usuario) => {
-//     return obtenerCursos(usuario.id)
-// })
-// .then((cursos) => {
-//     return obtenerNotas(cursos)
-// })
-// .then((notas) => {
-//     console.log(notas)
-// })
-// .catch((error) => {
-//     console.log(error)
-// })
+// // login(usuario).then((usuario) => {
+// //     return obtenerCursos(usuario.id)
+// // })
+// // .then((cursos) => {
+// //     return obtenerNotas(cursos)
+// // })
+// // .then((notas) => {
+// //     console.log(notas)
+// // })
+// // .catch((error) => {
+// //     console.log(error)
+// // })
 
-// async function mostrarNotas(){
-//     try {
-//     const usuario = await login(usuario)
-//     const cursos = await obtnerCursos(usuario.id)
-//     const notas = await obtenerNotas(cursos)
-//     console.log(notas)
-//     }
-//     catch(error) {
-//         console.log()
-//     }
+// // async function mostrarNotas(){
+// //     try {
+// //     const usuario = await login(usuario)
+// //     const cursos = await obtnerCursos(usuario.id)
+// //     const notas = await obtenerNotas(cursos)
+// //     console.log(notas)
+// //     }
+// //     catch(error) {
+// //         console.log()
+// //     }
+// // }
+
+// function obtenerClima() {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve("22°c - Soleado")
+//         }, 2000)
+//     })
 // }
 
-function obtenerClima() {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve("22°c - Soleado")
-        }, 2000)
-    })
-}
+// // con then()
+// obtenerClima().then((clima) => {
+//     console.log(clima)
+// });
 
-// con then()
-obtenerClima().then((clima) => {
-    console.log(clima)
-});
+// // con async/await
+// async function mostrarClima() {
+//     const clima = await obtenerClima()
+//     console.log(clima)
+// }
 
-// con async/await
-async function mostrarClima() {
-    const clima = await obtenerClima()
-    console.log(clima)
-}
+// mostrarClima()
 
-mostrarClima()
+// function consultarSaldo() {
+//     return new Promise((resolve) => {
+//     setTimeout(() => {
+//         resolve(125000)
+//     }, 3000);
+// });
+// }
 
-function consultarSaldo() {
-    return new Promise((resolve) => {
-    setTimeout(() => {
-        resolve(125000)
-    }, 3000);
-});
-}
+// async function mostrarSaldo(){
+//     const saldo = await consultarSaldo()
+//     console.log(`Su saldo es: $${saldo}`)
+// }
+// mostrarSaldo()
 
-async function mostrarSaldo(){
-    const saldo = await consultarSaldo()
-    console.log(`Su saldo es: $${saldo}`)
-}
-mostrarSaldo()
+// function iniciarSesion() {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve("Bienvenida, Irina")
+//         }, 2000)
+//     })
+// }
+// async function mostrarInicio() {
+//     const mensaje = await iniciarSesion()
+//     console.log(mensaje)
+// }
+// mostrarInicio()
 
-function iniciarSesion() {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve("Bienvenida, Irina")
-        }, 2000)
-    })
-}
-async function mostrarInicio() {
-    const mensaje = await iniciarSesion()
-    console.log(mensaje)
-}
-mostrarInicio()
+// function obtenerUsuario(){
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve({
+//                 id: 1,
+//                 nombre: "Maria",
+//                 edad: 25
+//             })
+//         }, 3000);
+//     })
+// }
+// async function mostrarUsuario(){
+//     console.log("Consultando usuario...")
+//     const usuario = await obtenerUsuario()
+//     console.log(usuario)
+// }
+// mostrarUsuario()
 
-function obtenerUsuario(){
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve({
-                id: 1,
-                nombre: "Maria",
-                edad: 25
-            })
-        }, 3000);
-    })
+async function prueba(){
+const respuesta = await fetch("https://jsonplaceholder.typicode.com/users")
+console.log(respuesta)
 }
-async function mostrarUsuario(){
-    console.log("Consultando usuario...")
-    const usuario = await obtenerUsuario()
-    console.log(usuario)
-}
-mostrarUsuario()
+prueba()
