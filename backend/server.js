c84c37710bd56ce6a3065235562e40ea2d0dbad7
IAgretti <irinagretti@gmail.com>
2026-09-16 18:55:50 -0300

    feat: realizo comentarios en el código para repaso de lo que hace cada intrucción

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,24 @@
-require("dotenv").config()
+require("dotenv").config() // Importamos dotenv para poder usar las variables de entorno definidas en el archivo .env
 
-const express = require("express")
-const cors = require("cors")
+const express = require("express") // Importamos express para poder crear el servidor de la API
+const cors = require("cors") // Importamos cors para poder permitir el acceso a la API desde otros dominios
 
-const conectarDB = require("./config/database")
-const alumnosRoutes = require("./routes/alumnos.routes")
+const conectarDB = require("./config/database") // Importamos la función conectarDB para poder conectarnos a la base de datos
+const alumnosRoutes = require("./routes/alumnos.routes") // Importamos las rutas de alumnos para poder usarlas en el servidor
 
-const app = express()
+const app = express() // Creamos una instancia de express para poder usar sus funcionalidades
 
-app.use(express.json())
-app.use(cors())
-app.use("/alumnos", alumnosRoutes)
+app.use(express.json()) // Middleware para poder recibir datos en formato JSON en las peticiones de la API
+app.use(cors()) // Middleware para permitir el acceso a la API desde otros dominios
+app.use("/alumnos", alumnosRoutes) 
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT // Definimos el puerto en el que se ejecutará el servidor, tomando el valor de la variable de entorno PORT definida en el archivo .env
 
-conectarDB()
+conectarDB() // Llamamos a la función conectarDB para conectarnos a la base de datos
 
 console.log("Ejecutado con nodemon")
 
-app.listen(PORT, () => {
+app.listen(PORT, () => { // Iniciamos el servidor en el puerto definido en la variable PORT
     console.log(`Servidor funcionando en http://localhost:${PORT}`)
 })
 
--- frontend/JS/alumnos.js
+++ frontend/JS/alumnos.js
@@ -1,23 +1,23 @@
-const { estimatedDocumentCount } = require("../../backend/models/Alumno")
-
 const formulario = document.querySelector("#formulario")
-const mensaje = document.querySelector("#mensaje")
+// const mensaje = document.querySelector("#mensaje")
 const listaAlumnos = document.querySelector("#listaAlumnos")
-let alumnoEditandoLegajo = null
-let alumnoEditar = null
+let alumnoEditandoLegajo = null // Variable para almacenar el legajo del alumno que se está editando
+let alumnoEditar = null // Variable para almacenar los datos del alumno que se está editando
 const btnCancelar = document.querySelector("#btnCancelar")
-btnCancelar.style.display = "none"
+btnCancelar.style.display = "none"  // Ocultar el botón de cancelar al inicio
 const btnGuardar = document.querySelector("#btnGuardar")
-const API_ALUMNOS = "http://localhost:3000/alumnos"
+const API_ALUMNOS = "http://localhost:3000/alumnos" // URL de la API de alumnos
 
-formulario.addEventListener("submit", async function (event) {
+formulario.addEventListener("submit", async function (event) { // hacemos async la función para poder usar await dentro de ella
     event.preventDefault();
 
+    // Cargamos los valores de los campos del formulario
     const legajo = document.querySelector("#legajo").value.trim()
     const nombre = document.querySelector("#nombre").value.trim()
     const carrera = document.querySelector("#carrera").value.trim()
     const correo = document.querySelector("#correo").value.trim()
 
+    // validaciones de los campos del formulario
     if (legajo === "" || nombre === "" || carrera === "" || correo === "") {
         mostrarMensaje("Todos los campos son obligatorios", "mje-error")
         return
@@ -33,43 +33,43 @@ formulario.addEventListener("submit", async function (event) {
         return
     }
 
-    // Post
+    // Si alumnoEditandoLegajo es null, significa que estamos creando un nuevo alumno, por lo que hacemos un POST.
     if (alumnoEditandoLegajo === null) {
-        const alumno = {
+        const alumno = { // Creamos un objeto con los datos del alumno
             legajo: Number(legajo),
             nombre: nombre,
             carrera: carrera,
             correo: correo
         }
-        const respuesta = await fetch(API_ALUMNOS, {
+        const respuesta = await fetch(API_ALUMNOS, { // Hacemos un POST a la API de alumnos
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify(alumno)
+            body: JSON.stringify(alumno) // Convertimos el objeto alumno a JSON
         })
-        if (!respuesta.ok) {
-            mostrarMensaje("No se puso guardar el alumno", "mje-error")
+        if (!respuesta.ok) { // Si la respuesta no es ok, mostramos un mensaje de error
+            mostrarMensaje("No se pudo guardar el alumno", "mje-error")
             return
         }
         mostrarMensaje("Alumno guardado correctamente", "mje-exito")
-    } else { // Put
-        const datosActuales = {
+    } else { // Si alumnoEditandoLegajo no es null, significa que estamos editando un alumno existente, por lo que hacemos un PUT.
+        const datosActuales = { // Creamos un objeto con los datos actuales del alumno que estamos editando
             nombre: nombre,
             carrera: carrera,
             correo: correo
         }
-        if (JSON.stringify(datosActuales) === JSON.stringify(alumnoEditar)){
+        if (JSON.stringify(datosActuales) === JSON.stringify(alumnoEditar)){ // Comparamos los datos actuales con los datos del alumno que estamos editando, si son iguales, mostramos un mensaje de advertencia y salimos de la función
             mostrarMensaje("No se realizaron cambios", "mje-adv")
             return
         }
-        const respuesta = await fetch(`${API_ALUMNOS}/${alumnoEditandoLegajo}`, {
+        const respuesta = await fetch(`${API_ALUMNOS}/${alumnoEditandoLegajo}`, { // Hacemos un PUT a la API de alumnos con el legajo del alumno que estamos editando
             method: "PUT",
             headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                nombre: nombre,
+                nombre: nombre, // Enviamos los datos actualizados del alumno
                 carrera: carrera,
                 correo: correo
             })
@@ -78,24 +78,29 @@ formulario.addEventListener("submit", async function (event) {
             mostrarMensaje("No se pudo actualizar el alumno", "mje-error")
             return
         }
-        alumnoEditandoLegajo = null
-        alumnoEditar = null
-        btnGuardar.textContent = "Guardar Alumno"
-        document.querySelector("#legajo").disabled = false
+        alumnoEditandoLegajo = null // Reiniciamos la variable alumnoEditandoLegajo a null para indicar que ya no estamos editando ningún alumno
+        alumnoEditar = null // Reiniciamos la variable alumnoEditar a null para indicar que ya no estamos editando ningún alumno
+        btnGuardar.textContent = "Guardar Alumno" // Cambiamos el texto del botón de guardar a "Guardar Alumno"
+        document.querySelector("#legajo").disabled = false // Habilitamos el campo de legajo para que se pueda editar
+        
         mostrarMensaje("Alumno actualizado correctamente", "mje-exito")
     }
-    await actualizarListaAlumnos()
+    await actualizarListaAlumnos() // Actualizamos la lista de alumnos después de guardar o actualizar un alumno
     formulario.reset()
 });
 
 
-async function obtenerAlumnos() {
-    const respuesta = await fetch(API_ALUMNOS)
-    const alumnos = await respuesta.json()
-    return alumnos
+async function obtenerAlumnos() { // Función para obtener la lista de alumnos desde la API
+    const respuesta = await fetch(API_ALUMNOS)  // Hacemos un GET a la API de alumnos
+    if (!respuesta.ok) { // Si la respuesta no es ok, mostramos un mensaje de error
+        mostrarMensaje("No se pudo obtener la lista de alumnos", "mje-error")
+        return []
+    }
+    const alumnos = await respuesta.json() // Convertimos la respuesta a JSON y la guardamos en la variable alumnos
+    return alumnos // Devolvemos la lista de alumnos
 }
 
-function mostrarAlumnos(alumnos) {
+function mostrarAlumnos(alumnos) { // Función para mostrar la lista de alumnos en la tabla
     listaAlumnos.innerHTML = ""
     for (const alumno of alumnos) {
         listaAlumnos.innerHTML += `
@@ -107,7 +112,7 @@ function mostrarAlumnos(alumnos) {
             <td>
                 <button 
                 class="btn-editar" 
-                data-legajo="${alumno.legajo}"
+                data-legajo="${alumno.legajo}" // Agregamos un atributo data-legajo al botón de editar para poder identificar qué alumno se está editando
                 title="Editar alumno">
                 <i class="fa-solid fa-pen"></i>
                 </button>
@@ -123,8 +128,8 @@ function mostrarAlumnos(alumnos) {
     }
 }
 
- async function eliminarAlumno(legajo) {
-    const respuesta = await fetch(`${API_ALUMNOS}/${legajo}`, {
+ async function eliminarAlumno(legajo) { // Función para eliminar un alumno desde la API
+    const respuesta = await fetch(`${API_ALUMNOS}/${legajo}`, { // Hacemos un DELETE a la API de alumnos con el legajo del alumno que queremos eliminar
         method: "DELETE"
     })    
     if (!respuesta.ok) {
@@ -132,7 +137,7 @@ function mostrarAlumnos(alumnos) {
         return
     }
     
-    if (alumnoEditandoLegajo === legajo){
+    if (alumnoEditandoLegajo === legajo){ // Si el alumno que estamos editando es el mismo que estamos eliminando, reiniciamos el formulario y las variables de edición
         formulario.reset()
         alumnoEditar = null
         alumnoEditandoLegajo = null
@@ -144,67 +149,67 @@ function mostrarAlumnos(alumnos) {
     await actualizarListaAlumnos()
 }
 
-async function actualizarListaAlumnos() {
+async function actualizarListaAlumnos() { // Función para actualizar la lista de alumnos en la tabla
     const alumnos = await obtenerAlumnos()
     mostrarAlumnos(alumnos)
 }
 
-listaAlumnos.addEventListener("click", (e) => {
-    const boton_el = e.target.closest(".btn-eliminar")
-    if (boton_el) {
+listaAlumnos.addEventListener("click", (e) => { // Escuchamos el evento click en la tabla de alumnos para poder editar o eliminar un alumno
+    const boton_el = e.target.closest(".btn-eliminar") // Buscamos el botón de eliminar más cercano al elemento que se hizo click
+    if (boton_el) { // Si se hizo click en un botón de eliminar, obtenemos el legajo del alumno que queremos eliminar y mostramos un mensaje de confirmación
         const legajo = Number(boton_el.dataset.legajo)
         const confirmar = confirm("¿Está seguro de eliminar este alumno?")
         if (confirmar) {
         eliminarAlumno(legajo)
         }
     }
-    const boton_ed = e.target.closest(".btn-editar")
-    if (boton_ed) {
+    const boton_ed = e.target.closest(".btn-editar") // Buscamos el botón de editar más cercano al elemento que se hizo click
+    if (boton_ed) { // Si se hizo click en un botón de editar, obtenemos el legajo del alumno que queremos editar y llamamos a la función editarAlumno
         const legajo = Number(boton_ed.dataset.legajo)
         editarAlumno(legajo)
     }
 })
 
-async function editarAlumno(legajo) {
+async function editarAlumno(legajo) { // Función para editar un alumno desde la API
     const alumnos = await obtenerAlumnos()
-    const alumno = alumnos.find(alumno => alumno.legajo === legajo)
+    const alumno = alumnos.find(alumno => alumno.legajo === legajo) // Buscamos el alumno que queremos editar en la lista de alumnos obtenida desde la API
 
-    if (!alumno) {
+    if (!alumno) { // Si no encontramos el alumno, mostramos un mensaje de error y salimos de la función
         mostrarMensaje("Alumno no encontrado", "mje-error")
         return
     }
-    document.querySelector("#legajo").value = alumno.legajo;
-    document.querySelector("#legajo").disabled = true;
+    document.querySelector("#legajo").value = alumno.legajo; // Cargamos el legajo del alumno en el campo de legajo del formulario
+    document.querySelector("#legajo").disabled = true; // Deshabilitamos el campo de legajo para que no se pueda editar
     document.querySelector("#nombre").value = alumno.nombre;
     document.querySelector("#carrera").value = alumno.carrera;
     document.querySelector("#correo").value = alumno.correo;
 
-    alumnoEditar = {
+    alumnoEditar = { // Creamos un objeto con los datos del alumno que estamos editando para poder compararlos con los datos actuales del formulario y saber si se realizaron cambios
         nombre: alumno.nombre,
         carrera: alumno.carrera,
         correo: alumno.correo
     }
 
-    alumnoEditandoLegajo = alumno.legajo;
-    btnCancelar.style.display ="inline-block"
-    btnGuardar.textContent = "Actualizar Alumno"
-    document.querySelector("#nombre").focus()
+    alumnoEditandoLegajo = alumno.legajo; // Guardamos el legajo del alumno que estamos editando en la variable alumnoEditandoLegajo para poder usarlo en la función de submit del formulario
+    btnCancelar.style.display ="inline-block" // Mostramos el botón de cancelar para que el usuario pueda cancelar la edición
+    btnGuardar.textContent = "Actualizar Alumno" // Cambiamos el texto del botón de guardar a "Actualizar Alumno" para indicar que estamos editando un alumno
+    document.querySelector("#nombre").focus() // Ponemos el foco en el campo de nombre para que el usuario pueda empezar a editar los datos del alumno
 }
 
-function cancelarEdicion(){
+function cancelarEdicion(){ // Función para cancelar la edición de un alumno
     formulario.reset()
     alumnoEditandoLegajo = null
     alumnoEditar = null
     btnGuardar.textContent = "Guardar Alumno"
     document.querySelector("#legajo").disabled = false
-    btnCancelar.style.display = "none"
-    document.querySelector("#legajo").focus()
+    btnCancelar.style.display = "none" // Ocultamos el botón de cancelar al cancelar la edición
+    document.querySelector("#legajo").focus() // Ponemos el foco en el campo de legajo para que el usuario pueda empezar a ingresar un nuevo alumno
 }
 
-btnCancelar.addEventListener("click", cancelarEdicion)
+btnCancelar.addEventListener("click", cancelarEdicion) // Escuchamos el evento click en el botón de cancelar para llamar a la función cancelarEdicion
 
-async function iniciar(){
+async function iniciar(){ // Función para iniciar la aplicación y cargar la lista de alumnos al cargar la página
     await actualizarListaAlumnos()
 }
 
-iniciar()
+iniciar() // Llamamos a la función iniciar al cargar la página
